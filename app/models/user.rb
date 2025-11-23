5d6043d7b960284d1d5f707ba25cc106b9d9e68f
kaji-hub <u5tone.kaji@gmail.com>
2025-11-23 18:08:29 +0900

    ユーザー管理機能導入

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+  validates :nickname, presence: true, length: { maximum: 40 }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字で入力してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :birth_date, presence: true
+
+  # パスワードの半角英数字混合バリデーション
+  validate :password_complexity
+
+  def password_complexity
+    return if password.blank?
+
+    return if password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/)
+
+    errors.add(:password, 'は半角英数字混合で入力してください')
+  end
+end
