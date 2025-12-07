892a3723d74ee1100271fe1b1410ec6f464afa0c
kaji-hub <u5tone.kaji@gmail.com>
2025-12-07 18:30:27 +0900

    specファイル修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,11 +6,11 @@ class OrderAddress
     validates :item_id
     validates :user_id
     validates :token
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'は半角「3桁-4桁」形式で入力してください' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は半角「3桁-4桁」形式で入力してください' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :city
-    validates :house_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は半角数字10〜11桁で入力してください' }
+    validates :city, presence: true
+    validates :house_number, presence: true
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'は半角数字10〜11桁で入力してください' }
   end
 
   def save
