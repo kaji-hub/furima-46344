eb53385eb7547c951c8f4617936c6bb601256d14
kaji-hub <u5tone.kaji@gmail.com>
2025-12-15 22:08:27 +0900

    エラー表示修正４

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,11 +6,12 @@ class OrderAddress
     validates :item_id
     validates :user_id
     validates :token
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'は半角「3桁-4桁」形式で入力してください' }
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :house_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は半角数字10〜11桁で入力してください' }
+    validates :phone_number, length: { minimum: 10, maximum: 11, too_short: 'is too short', too_long: 'is invalid. Input only number' }
+    validates :phone_number, format: { with: /\A\d+\z/, message: 'is invalid. Input only number' }, allow_blank: true
   end
 
   def save
